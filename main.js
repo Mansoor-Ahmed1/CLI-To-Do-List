809506c2329df94c3c910b2ed4e56f71956b7d73
Mansoor Ahmed <mansoor.ahmed11521@gmail.com>
2024-06-14 15:39:58 +0500

    removal of tasks

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import inquirer from "inquirer";
-export let to_do_tasks = [];
-let condition = true;
+let to_do_tasks = [];
+let condition_for_addition = true;
+let condition_for_removal = true;
 console.log(to_do_tasks);
 let ask_to_add_task = await inquirer.prompt([
     {
@@ -11,7 +12,7 @@ let ask_to_add_task = await inquirer.prompt([
     }
 ]);
 if (ask_to_add_task.ask_to_add == true) {
-    while (condition) {
+    while (condition_for_addition) {
         let addTask = await inquirer.prompt([
             {
                 name: "todo",
@@ -35,11 +36,41 @@ if (ask_to_add_task.ask_to_add == true) {
             }
         ]);
         to_do_tasks.push(addTask.todo); // adding tasks
-        condition = addTask.add_todo; // updating variable condition if user don't want to add anymore tasks
+        condition_for_addition = addTask.add_todo; // updating variable condition if user don't want to add anymore tasks
         console.log(to_do_tasks); //consolling the list with the uer's addition.
     }
 }
 // consolling each item line-by-line
 for (let i = 0; i < to_do_tasks.length; i++) {
     console.log(to_do_tasks[i]);
+}
+//Removing a task from the To-Do List
+let ask_to_remove_task = await inquirer.prompt([
+    {
+        name: "remove_item",
+        type: "confirm",
+        default: "false",
+        message: "Would you like to delete any item from To-Do List?",
+    }
+]);
+if (ask_to_remove_task.remove_item === true) {
+    while (condition_for_removal) {
+        let remove_item = await inquirer.prompt([
+            {
+                name: "remove_item",
+                type: "list",
+                choices: to_do_tasks,
+                message: "Which which of the task would you like to delete from To-Do List?",
+            },
+            {
+                name: "again_remove_item",
+                type: "confirm",
+                default: false,
+                message: "Would you like to remove another item from the list?",
+            }
+        ]);
+        condition_for_removal = remove_item.again_remove_item;
+        to_do_tasks = to_do_tasks.filter(item => item !== remove_item.remove_item);
+        console.log(to_do_tasks);
+    }
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import inquirer from "inquirer"
 
-export let to_do_tasks: string[] = []
-let condition: boolean = true
+let to_do_tasks: string[] = []
+
+let condition_for_addition: boolean = true
+let condition_for_removal: boolean = true
 
 console.log(to_do_tasks)
 
@@ -11,15 +13,13 @@ let ask_to_add_task = await inquirer.prompt(
             name: "ask_to_add",
             type: "confirm",
             message: "Do you want to add anything to your To-Do List?",
-            default: "true",
+            default: true,
         }
     ]
 )
 
 if (ask_to_add_task.ask_to_add == true) {
-
-
-    while (condition) {
+    while (condition_for_addition) {
         let addTask = await inquirer.prompt(
             [
                 {
@@ -46,12 +46,49 @@ if (ask_to_add_task.ask_to_add == true) {
         );
 
         to_do_tasks.push(addTask.todo) // adding tasks
-        condition = addTask.add_todo // updating variable condition if user don't want to add anymore tasks
+        condition_for_addition = addTask.add_todo // updating variable condition if user don't want to add anymore tasks
         console.log(to_do_tasks) //consolling the list with the uer's addition.
     }
 }
 
 // consolling each item line-by-line
-for(let i = 0 ; i < to_do_tasks.length; i++){
+for (let i = 0; i < to_do_tasks.length; i++) {
     console.log(to_do_tasks[i])
+}
+
+//Removing a task from the To-Do List
+let ask_to_remove_task = await inquirer.prompt(
+    [
+        {
+            name: "remove_item",
+            type: "confirm",
+            default: false,
+            message: "Would you like to delete any item from To-Do List?",
+        }
+    ]
+)
+
+if (ask_to_remove_task.remove_item === true) {
+    while (condition_for_removal) {
+        let remove_item = await inquirer.prompt(
+            [
+                {
+                    name: "remove_item",
+                    type: "list",
+                    choices: to_do_tasks,
+                    message: "Which which of the task would you like to delete from To-Do List?",
+                },
+                {
+                    name: "again_remove_item",
+                    type: "confirm",
+                    default: false,
+                    message: "Would you like to remove another item from the list?",
+                }
+            ]
+        )
+
+        condition_for_removal = remove_item.again_remove_item
+        to_do_tasks = to_do_tasks.filter(item => item !== remove_item.remove_item);
+        console.log(to_do_tasks)
+    }
 }
